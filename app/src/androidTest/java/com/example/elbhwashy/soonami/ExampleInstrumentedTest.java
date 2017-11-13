83fd3c9470f1f0ea4f8e59416aa6143e104afae7
sobhy mohamed mohamed elsayed <30624911+elbhwashy@users.noreply.github.com>
2017-11-13 23:31:32 +0200

    first commit

---
+++ app/src/androidTest/java/com/example/elbhwashy/soonami/ExampleInstrumentedTest.java
@@ -0,0 +1,26 @@
+package com.example.elbhwashy.soonami;
+
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+public class ExampleInstrumentedTest {
+    @Test
+    public void useAppContext() throws Exception {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getTargetContext();
+
+        assertEquals("com.example.elbhwashy.soonami", appContext.getPackageName());
+    }
+}
